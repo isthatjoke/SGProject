1b7a05d7e3a8bc79b0c51fe5ad5359bc83c63c0b
Vinogradov Andrey <ezh24@yandex.ru>
2021-04-04 23:35:35 +0300

    post-models-1

---
+++ post/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from authapp.models import HubUser, HubUserProfile
+from hub.models import HubCategory, Hub
+
+# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=200, verbose_name='название', )
+    short_desc = models.CharField(max_length=200, verbose_name='краткое описание', )
+    post_text = models.TextField(verbose_name='пост', blank=False, null=True)
+    hub_category = models.ForeignKey(HubCategory, related_name='hub_category',
+                                     verbose_name='подкатегория', on_delete=models.CASCADE, **NULLABLE)
+    published = models.BooleanField(default=False)
+    user_id = models.ForeignKey(HubUserProfile, related_name='user_id', on_delete=models.CASCADE, **NULLABLE)
+    created_at = models.DateTimeField(verbose_name='время создания', auto_now_add=True, )
+    updated_at = models.DateTimeField(verbose_name='время обновления', auto_now=True, )
+
+    # Метод возвращает посты конкретного пользователя
+    # pk - id пользователя
+    @staticmethod
+    def get_user_posts(pk):
+        return Post.objects.filter(user=pk).order_by('updated_at')
+
+    def __str__(self):
+        return f'{self.name} ({self.hub_category.name})'
+    
