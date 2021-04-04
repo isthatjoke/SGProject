19443668b49db7ab8b0b2b49620bd47b611991ea
Isenhaim <simonov.ev.al@gmail.com>
2021-04-04 14:55:00 +0300

    add template

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import hub.views as hub
+
 urlpatterns = [
+    path('', hub.main, name='main'),
     path('admin/', admin.site.urls),
 ]
