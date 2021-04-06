216a101d656ee3893d10ec3b44950b2f93f1b463
Vinogradov Andrey <ezh24@yandex.ru>
2021-04-07 01:03:23 +0300

    hub-menu-ver-1

--- hub/models.py
+++ hub/models.py
@@ -4,29 +4,47 @@ from django.db import models
 from authapp.models import HubUser
 
 NULLABLE = {'blank': True, 'null': True}
+
+
 # Create your models here.
 
 
 class Hub(models.Model):
-    name = models.CharField(max_length=150, verbose_name='название',)
-    description = models.CharField(max_length=500, verbose_name='описание', **NULLABLE,)
+    name = models.CharField(max_length=150, verbose_name='название', )
+    description = models.CharField(max_length=500, verbose_name='описание', **NULLABLE, )
 
 
 class HubCategory(models.Model):
-    name = models.CharField(max_length=150, verbose_name='название',)
-    short_desc = models.CharField(max_length=150, verbose_name='краткое описание',)
-    description = models.CharField(max_length=500, verbose_name='описание', **NULLABLE,)
-    tags = models.CharField(max_length=300, verbose_name='тэги', **NULLABLE,)
+    name = models.CharField(max_length=150, verbose_name='название', )
+    short_desc = models.CharField(max_length=150, verbose_name='краткое описание', )
+    description = models.CharField(max_length=500, verbose_name='описание', **NULLABLE, )
+    tags = models.CharField(max_length=300, verbose_name='тэги', **NULLABLE, )
     category = models.ForeignKey(Hub, related_name='categories', verbose_name='категории', on_delete=models.CASCADE,
                                  **NULLABLE)
-    created_at = models.DateTimeField(verbose_name='время создания', auto_now_add=True,)
-    updated_at = models.DateTimeField(verbose_name='время обновления', auto_now=True,)
+    created_at = models.DateTimeField(verbose_name='время создания', auto_now_add=True, )
+    updated_at = models.DateTimeField(verbose_name='время обновления', auto_now=True, )
 
 
 class HubCategoryUsers(models.Model):
     user = models.ForeignKey(HubUser, related_name='users', on_delete=models.CASCADE, **NULLABLE)
-    hub_category = models.ForeignKey(HubCategory, related_name='hubs', on_delete=models.CASCADE,)
+    hub_category = models.ForeignKey(HubCategory, related_name='hubs', on_delete=models.CASCADE, )
 
     @staticmethod
     def get_category_users(pk):
         return HubCategoryUsers.objects.filter(hub_category=pk).count()
+
+
+def get_hub_cats_dict():
+    '''
+    Функция формирует словарь result_dict, ключи словаря - это имена хабов,
+    значения - это объекты категорий хабов
+    :return: result_dict={'hub.name': HubCategoryObjects,....}
+    '''
+    result_dict = {}
+    hubs = Hub.objects.all()
+    for k in hubs:
+        list_cats = []
+        hubcats = HubCategory.objects.filter(category=k.id)
+        list_cats.append(hubcats)
+        result_dict[k] = list_cats
+    return result_dict
--- hub/views.py
+++ hub/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from hub.models import Hub, HubCategory, get_hub_cats_dict
 
 
 def main(request):
-    content = {}
+    head_menu_object_list = get_hub_cats_dict()
+    content = {
+        'head_menu_object_list': head_menu_object_list,
+
+    }
 
     return render(request, 'hub/index.html', context=content)
