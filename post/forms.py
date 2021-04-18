239cee27ff0808f515674a6ad9860601fbb66fcd
Timur Zverev <61158881+isthatjoke@users.noreply.github.com>
2021-04-19 00:15:40 +0300

    [fix]post-filtering-publishing[]

--- post/forms.py
+++ post/forms.py
@@ -4,6 +4,25 @@ from django import forms
 from post.models import Post
 
 
+class PostCreationForm(forms.ModelForm):
+    content = forms.CharField(label='Содержание', widget=CKEditorUploadingWidget(config_name='default'))
+
+
+    class Meta:
+        model = Post
+        exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+            if field_name in ('user_id', 'post_karma', 'status'):
+                field.widget = forms.HiddenInput()
+            if field_name == 'hub_category':
+                field.widget.attrs['required'] = True
+
+
 class PostEditForm(forms.ModelForm):
     content = forms.CharField(label='Содержание', widget=CKEditorUploadingWidget(config_name='default'))
 
