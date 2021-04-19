59ed16ce8bbe21479a1c2cc33df6d30d6763da63
Timur Zverev <61158881+isthatjoke@users.noreply.github.com>
2021-04-19 23:15:21 +0300

    database

--- backend/settings.py
+++ backend/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sgproject',
+        'USER': 'postgres',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
