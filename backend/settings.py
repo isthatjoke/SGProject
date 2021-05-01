d5d8bbfd5c762f935f2befafb5c3c70b59d6c0f7
is_that_joke <leniviy7@gmail.com>
2021-05-01 22:40:43 +0300

    settings-fix

--- backend/settings.py
+++ backend/settings.py
@@ -149,7 +149,7 @@ CKEDITOR_REQUIRE_STAFF = False
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
-        'height': 300,
-        'width': 600,
+        'height': '25em',
+        'width': '100%',
     },
 }
