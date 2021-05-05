231351a6959dd7b53d9754c0ad72092cde62dc36
is_that_joke <leniviy7@gmail.com>
2021-05-05 16:20:31 +0300

    settings-fix

--- backend/settings.py
+++ backend/settings.py
@@ -140,8 +140,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DOMAIN_NAME = 'http://isthatjoke.xyz'
-SERVER_NAME = 'http://isthatjoke.xyz'
+DOMAIN_NAME = 'http://www.isthatjoke.xyz'
+SERVER_NAME = 'http://www.isthatjoke.xyz'
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "uploads/"
