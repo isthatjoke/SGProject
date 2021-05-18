125017a1ba5144822a1a4267d0f2f6b0c6bfe641
is_that_joke <leniviy7@gmail.com>
2021-05-18 18:00:40 +0300

    settings-fix

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zl&%@w5tpy6e*4@@lg1=#p0%9ta%h#sj)^a0$)yyz*^nf$odi+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
