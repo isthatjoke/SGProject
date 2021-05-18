fed67de762a3196c9db52935665209ffda5034c9
is_that_joke <leniviy7@gmail.com>
2021-05-18 20:12:42 +0300

    settings-fix

--- backend/settings.py
+++ backend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sgproject',
+        'NAME': 'sgproject2',
         'USER': 'postgres',
     }
     # 'default': {
